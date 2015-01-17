53931efd85536ce5bdc24d735ded7b9fd3960b80
1678 Programming <1678programming@gmail.com>
2015-01-17 11:50:10 -0800

    Separated FileSave into .h and .cpp

--- src/FileSave.cpp
+++ src/FileSave.cpp
@@ -1,7 +1,6 @@
 #include "FileSave.h"
 
 FileSave::FileSave(string fileName) {
-	logging = false;
 	filename = "/home/admin/logs/" + fileName;
 	curTime = new Timer();
 	curTime->Reset();
@@ -16,32 +15,49 @@ void FileSave::reset() {
 	curTime->Start();
 }
 
-void FileSave::log(string value) {
-	if (!logging) {
-		logging = true;
-		outfile.is_open();
-		outfile << value << endl;
-	}
-	outfile << value << endl;
+void FileSave::logTime()
+{
+	/**LOG THE TIME**/
+	time_t rawtime;
+	tm* timeinfo;
+	char buffer[80];
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	std::strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
+	timeval tv;
+	gettimeofday(&tv, NULL);
+	int timeMS=tv.tv_usec/1000;
+	char msString[25];
+	sprintf(msString, "%03d", timeMS);
+	outfile << buffer << '.' << msString << ",";
+	/****************/
 }
+
+void FileSave::logRobotInit()
+{
+	log("Joystick1,Joystick2,DriveSolenoid,DriveEncoder1Val,DriveEncoder2Val,DriveEncoder1Rate,DriveEncoder2Rate");
+}
+
+void FileSave::log(string value) {
+	logTime();
+	outfile << value << '\n';
+}
+	
 void FileSave::start() {
-	logging = true;
 	outfile.open(filename, ios::app);
 }
+
 void FileSave::flush() {
-	logging = false;
 	outfile.close();
 }
-void FileSave::logDriveBase(Joystick* j1, Joystick* j2, Encoder* e1,
-		Encoder* e2, DoubleSolenoid* ds) {
-	start();
-	log("-------------------------");
-	log("Joystick 1 Value: " + to_string(j1->GetY()));
-	log("Joystick 2 Value: " + to_string(j2->GetY()));
-	log("Solenoid Value: " + to_string(ds->Get()));
-	log("Encoder 1 Value: " + to_string(e1->Get()));
-	log("Encoder 2 Value: " + to_string(e2->Get()));
-	log("Encoder 1 Rate: " + to_string(e1->GetRate()));
-	log("Encoder 2 Rate: " + to_string(e2->GetRate()));
-	flush();
+void FileSave::logRobot(Joystick* j1, Joystick* j2, Encoder* e1, Encoder* e2, DoubleSolenoid* ds)
+{
+		string to_log = to_string(j1->GetY()) + "," +
+					to_string(j2->GetY()) + "," +
+					to_string(ds->Get()) + "," +
+					to_string(e1->Get()) + "," +
+					to_string(e2->Get()) + "," +
+					to_string(e1->GetRate()) + "," +
+					to_string(e2->GetRate());
+	log(to_log);
 }
--- src/FileSave.h
+++ src/FileSave.h
@@ -15,66 +15,17 @@ class FileSave {
 	string filename;
 	Timer *curTime;
 public:
-	FileSave(string fileName){
-		filename = "/home/admin/logs/" + fileName;
-		curTime = new Timer();
-		curTime->Reset();
-		curTime->Start();
-	}
-	~FileSave(){}
+	FileSave(string fileName);
+	~FileSave();
 
-	void reset(){
-		curTime->Reset();
-		curTime->Start();
-	}
+	void reset();
 
-	void log(string value) {
-		logTime();
-		outfile << value << '\n';
-	}
-	void start()
-	{
-		outfile.open(filename, ios::app);
-	}
-	void flush()
-	{
-		outfile.close();
-	}
-	void logTime()
-	{
-		/**LOG THE TIME**/
-		time_t rawtime;
-		tm* timeinfo;
-		char buffer[80];
-
-		time(&rawtime);
-		timeinfo = localtime(&rawtime);
-
-		std::strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
-		timeval tv;
-		gettimeofday(&tv, NULL);
-		int timeMS=tv.tv_usec/1000;
-		char msString[25];
-		sprintf(msString, "%03d", timeMS);
-		outfile << buffer << '.' << msString << ",";
-		/****************/
-	}
-	void logRobotInit()
-	{
-		log("Joystick1,Joystick2,DriveSolenoid,DriveEncoder1Val,DriveEncoder2Val,DriveEncoder1Rate,DriveEncoder2Rate");
-	}
-	void logRobot(Joystick* j1, Joystick* j2, Encoder* e1, Encoder* e2, DoubleSolenoid* ds)
-	{
-
-		string to_log = to_string(j1->GetY()) + "," +
-						to_string(j2->GetY()) + "," +
-						to_string(ds->Get()) + "," +
-						to_string(e1->Get()) + "," +
-						to_string(e2->Get()) + "," +
-						to_string(e1->GetRate()) + "," +
-						to_string(e2->GetRate());
-		log(to_log);
-	}
+	void log(string value);
+	void start();
+	void flush();
+	void logTime();
+	void logRobotInit();
+	void logRobot(Joystick* j1, Joystick* j2, Encoder* e1, Encoder* e2, DoubleSolenoid* ds);
 };
 
 
