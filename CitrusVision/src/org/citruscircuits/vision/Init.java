b8739a96cc55adad7498f3944dfe67fb3e9faec0
Kylestach1678 <Kylestach99@gmail.com>
2015-01-21 20:37:40 -0800

    added OpenCV to vision

---
+++ CitrusVision/src/org/citruscircuits/vision/Init.java
@@ -0,0 +1,25 @@
+package org.citruscircuits.vision;
+
+//wouldn't it be cool if you could import org.opencv.*.*?
+import org.opencv.*;
+import org.opencv.calib3d.*;
+import org.opencv.contrib.*;
+import org.opencv.core.*;
+import org.opencv.features2d.*;
+import org.opencv.gpu.*;
+import org.opencv.highgui.*;
+import org.opencv.imgproc.*;
+import org.opencv.ml.*;
+import org.opencv.objdetect.*;
+import org.opencv.photo.*;
+import org.opencv.utils.*;
+import org.opencv.video.*;
+
+public class Init {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+}
