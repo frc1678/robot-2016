133c34bc71215bab5d8ff085a751e1048399d2e1
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2016-02-21 17:13:27 -0800

    Re-add short shot in auto

--- frc1678/frc1678.cpp
+++ frc1678/frc1678.cpp
@@ -135,6 +135,11 @@ void CitrusRobot::TeleopPeriodic() {
     shootable_ = true;
     start_climb_ = false;
   }
+  if (short_pos_->ButtonClicked()) {
+    subsystems_.arm.GoToAutoShot();
+    shootable_ = true;
+    start_climb_ = false;
+  }
   if (run_intake_until_->ButtonPressed()) {
     subsystems_.arm.SetIntake(IntakeGoal::FORWARD_UNTIL);
   } else if (run_intake_forever_->ButtonClicked()) {
