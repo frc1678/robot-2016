072026ffc68d05cc01e238b4966e59ee57aa76a7
Lucas Solomon <amannababanana@gmail.com>
2016-03-20 20:59:55 -0700

    works with camera on 1 or 2

--- vision/vision_main.cpp
+++ vision/vision_main.cpp
@@ -5,19 +5,26 @@
 #include <thread>
 #include "vision_utils.h"
 #include <iostream>
+#include <errno.h>
+#include <stdio.h>
 
 int main() {
   ObjectTracker tracker = ObjectTracker();
   cv::VideoCapture camera;
   camera.open(1);
+  if(!camera.isOpened()) camera.open(2);
+  if(!camera.isOpened()) return 0;
+
   std::thread sender(vision::startSending);
   while (true) {
     cv::Mat image;
     Time captureTime = muan::now();
-    camera>>image;
+    std::cout<<camera.grab()<<std::endl;
+    if(!camera.read(image)) return 0;
     TrackerResults position = tracker.Update(image);
     position.time_captured = captureTime;
     vision::updateData(image, position);
+    image.release();
   }
   sender.join();
   return 0;
