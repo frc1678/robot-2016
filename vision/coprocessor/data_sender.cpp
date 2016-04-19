d2389853c0ceca54dc3e56166c6423cac0b70ad5
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2016-04-19 08:37:57 -0700

    Fix vision code
    CitrusSocket::Send() with a SerializedData object doesn't work, so this
    commit sends a string instead.

--- vision/coprocessor/data_sender.cpp
+++ vision/coprocessor/data_sender.cpp
@@ -40,7 +40,7 @@ void sendData() {
   position_mutex.unlock();
   // Send the data
   try {
-    connection.Send(data, Destination("roborio-1678-frc.local", 9999));
+    connection.Send(to_json(data).dump().c_str(), Destination("roborio-1678-frc.local", 9999));
   } catch (...) {
     std::cout<<"no connection"<<std::endl;
   }
